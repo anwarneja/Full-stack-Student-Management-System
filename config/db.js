b331a016a168ed3c91b94416227e112a38c793cb
anwarneja <anwarneja55@gmail.com>
2025-03-07 06:46:08 +0300

    commit

--- config/db.js
+++ config/db.js
@@ -4,8 +4,9 @@ require('dotenv').config();
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    password: process.env.DB_PASSWORD, // Match your .env key
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306 // Use port from .env, default to 3306
 });
 
 db.connect((err) => {
